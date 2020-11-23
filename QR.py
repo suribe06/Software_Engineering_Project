02979c96dc15f967cf9bc68a255f81110dce814b
suribe06 <santiu990506@gmail.com>
2020-11-22 21:55:45 -0500

    Avance 9:55 pm

--- QR.py
+++ QR.py
@@ -1,9 +1,15 @@
 import qrcode
 
-data =  {'nombre' : 'Santiago', 'edad' : 21, 'CC': 1144107262}
-img = qrcode.make(data)
-filename = "QR_Code.png"
-#f = open("QR_Code.png", "wb")
-#img.save(f)
-#f.close()
-img.save(filename)
+def makeQR(data):
+    #Se crea el QR
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=3)
+    #Se agrega la informacion al qr
+    for key, value in data.items():
+        qr.add_data("{0} : {1}\n".format(key,value))
+    qr.make(fit=True)
+    #Se guarda el qr como imagen
+    img = qr.make_image(fill='black', back_color='white')
+    img.save('QR_Code.png')
