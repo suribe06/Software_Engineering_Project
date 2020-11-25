5e20b8637dd774e52a9428ba556e7e8cdc5c5dc6
suribe06 <santiu990506@gmail.com>
2020-11-25 03:27:00 -0500

    Encriptación terminada 3:26 am

---
+++ cryption.py
@@ -0,0 +1,13 @@
+import base64
+
+def encriptar(password):
+    password_encrypt = password.encode()
+    encoded = base64.b64encode(password_encrypt)
+    encoded = encoded.decode()
+    return encoded
+
+def decriptar(encoded):
+    p = encoded.encode()
+    decoded = base64.b64decode(p)
+    password_decrypt = decoded.decode('utf-8')
+    return password_decrypt
