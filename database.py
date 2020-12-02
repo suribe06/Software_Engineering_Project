26f850e7d5fa4c669e17d29c1bf0c441e4b066ac
suribe06 <santiu990506@gmail.com>
2020-12-02 02:10:56 -0500

    Historial de visitas civil terminado (sin filtrar y descargar) 2:10 am

--- database.py
+++ database.py
@@ -63,31 +63,31 @@ def registroP(usr,n,bar,cat,cor,dep,dir,mun,pasw,rsol,tel):
 			sessionDB.execute("INSERT INTO publica (username,Nit,barrio,categoria,correo,departamento,direccion,municipio,password,rsocial,telefono1,telefono2,telefono3) VALUES ('{0}',{1},'{2}','{10}','{3}','{4}','{5}','{6}','{7}','{8}',{9},NULL,NULL)".format(usr,n,bar,cor,dep,dir,mun,pasw,rsol,tel[0],cat))
 	return
 
-def regExam(id,n,td,nd):
-	"""
-	Entrada: un entero n el cual hace referencia al NIT de la entidad de salud, td el cual es un string que hace referencia al tipo de
-			 documento del civil al que se le va a registar el examen de COVID-19, y un entero nd el cual hace referencia al numero de
-			 documento del civil al que se le va a registar el examen de COVID-19.
-	Salida:
-	Funcionamiento: Mediante el uso de CQL se hacen 2 queries, la primera para obtener la entidad de salud con respecto a su NIT, y así verificar que
-					esta entidad existe, la segunda query es para obtener el civil con el numero de documento indicado y el tipo de documento ingresado
-					esta registrado en el sistema, de ser así se toma la fecha del día en el que se está registrando el examen y se registra la nit del
-					establecimiento de salud, la fecha en la que se realiza el registro, el resultado (antes de obtener un veredicto se guarda como Evaluando)
-					la fecha en la que se entrega el resultado (antes de obtener el veredicto se guarda como NULL), y el numero y tipo de documento del civil en cuestion
-	"""
-	sal = sessionDB.execute("SELECT username,nit from salud WHERE nit = {0} allow filtering".format(n))
-	person = sessionDB.execute("SELECT * from civil WHERE ndocumento = {0} and tdocumento = '{1}' allow filtering".format(nd,td))
-	if person.one() != None and	sal.one() != None:
-		dia = dt.datetime.today()
-		sessionDB.execute("INSERT INTO examenes (id,nit,ndocumento,efecha,resultado,rfecha,tdocumento) VALUES({6},{0},{1},'{2}-{3}-{4}','Evaluando',NULL,'{5}')".format(n,nd,dia.year,dia.month,dia.day,td,id))
-	return
+def regExam(id,n,td,nd,rsol):
+    """
+    Entrada: un entero n el cual hace referencia al NIT de la entidad de salud, td el cual es un string que hace referencia al tipo de
+             documento del civil al que se le va a registar el examen de COVID-19, y un entero nd el cual hace referencia al numero de
+             documento del civil al que se le va a registar el examen de COVID-19.
+    Salida:
+    Funcionamiento: Mediante el uso de CQL se hacen 2 queries, la primera para obtener la entidad de salud con respecto a su NIT, y así verificar que
+                    esta entidad existe, la segunda query es para obtener el civil con el numero de documento indicado y el tipo de documento ingresado
+                    esta registrado en el sistema, de ser así se toma la fecha del día en el que se está registrando el examen y se registra la nit del
+                    establecimiento de salud, la fecha en la que se realiza el registro, el resultado (antes de obtener un veredicto se guarda como Evaluando)
+                    la fecha en la que se entrega el resultado (antes de obtener el veredicto se guarda como NULL), y el numero y tipo de documento del civil en cuestion
+    """
+    sal = sessionDB.execute("SELECT username,nit from salud WHERE nit = {0} allow filtering".format(n))
+    person = sessionDB.execute("SELECT * from civil WHERE ndocumento = {0} and tdocumento = '{1}' allow filtering".format(nd,td))
+    if person.one() != None and    sal.one() != None:
+        dia = dt.datetime.now()
+        sessionDB.execute("INSERT INTO examenes (id,nit,ndocumento,efecha,resultado,rfecha,tdocumento,rsocial) VALUES({6},{0},{1},'{2}-{3}-{4}','Evaluando',NULL,'{5}','{7}')".format(n,nd,dia.year,dia.strftime("%m"),dia.strftime("%d"),td,id,rsol))
+    return
 
-def regResultExam(id,n,nd,res):
-	e = sessionDB.execute("SELECT ndocumento from examenes WHERE Id = {0}".format(id))
-	if e.one() != None:
-		dia = dt.datetime.today()
-		sessionDB.execute("UPDATE examenes SET rfecha = '{0}-{1}-{2}', resultado = '{6}' WHERE Id = {3} and Nit = {4} and ndocumento = {5}".format(dia.year,dia.month,dia.day,id,n,nd,res))
-	return
+def regResExam(id,n,nd,res,td):
+    e = sessionDB.execute("SELECT ndocumento from examenes WHERE Id = {0} and ndocumento = {1} and nit = {2} and tdocumento = '{3}'".format(id,nd,n,td))
+    if e.one() != None:
+        dia = dt.datetime.now()
+        sessionDB.execute("UPDATE examenes SET rfecha = '{0}-{1}-{2}', resultado = '{6}' WHERE Id = {3} and Nit = {4} and ndocumento = {5} and tdocumento = '{7}'".format(dia.year,dia.strftime("%m"),dia.strftime("%d"),id,n,nd,res,td))
+    return
 
 def getNd(usr):
     person = sessionDB.execute("SELECT ndocumento from civil where username = '{0}'".format(usr))
@@ -120,3 +120,47 @@ def editC(usr,pasw,ndoc,ape,bar,cor,dep,dire,mun,nac,nom,sex,tdoc,tel):
         exe+= exe1
         sessionDB.execute(exe)
     return
+
+def cuarentena(nd,td):
+    cuar = False
+    enfer = False
+    dia = dt.datetime.now()
+    dia -= dt.timedelta(days = 14)
+    sinres = sessionDB.execute("SELECT * from examenes where ndocumento = {0} and tdocumento = '{1}' and efecha >= '{2}-{3}-{4}' and resultado = 'Evaluando' allow filtering".format(nd,td,dia.year,dia.strftime("%m"),dia.strftime("%d")))
+    conres = sessionDB.execute("SELECT * from examenes where ndocumento = {0} and tdocumento = '{1}' and rfecha >= '{2}-{3}-{4}' and resultado = 'Positivo' allow filtering".format(nd,td,dia.year,dia.strftime("%m"),dia.strftime("%d")))
+    if sinres.one() != None: cuar = True
+    if conres.one() != None: enfer = True
+    return cuar, enfer
+
+def regVisita(i,ni,nd,td,nom,ape,tem,tap,rsol):
+    person = sessionDB.execute("SELECT nombres,apellidos from civil WHERE ndocumento = {0} and tdocumento = '{1}'allow filtering".format(nd,td))
+    if person.one() != None:
+        cuar,enfer = cuarentena(nd,td)
+        dia = dt.datetime.now()
+        temperatura = tem <= 37
+        ans = tap and temperatura and not(cuar) and not(enfer)
+        if ans == True:
+            sessionDB.execute("INSERT INTO visitas (id,nit,ndocumento,apellidos,fent,nombres,reason,rsocial,tapa,tdocumento,temp,veredict) VALUES({0},{1},{2},'{3}','{4}-{5}-{6}','{7}','NA','{11}',{8},'{9}',{10},True)".format(i,ni,nd,ape,dia.year,dia.strftime("%m"),dia.strftime("%d"),nom,tap,td,tem,rsol))
+        else:
+            razon = ''
+            if not(tap):
+                razon = razon + '- No porta tapabocas '
+            if not(temperatura):
+                razon = razon + '- Temperatura elevada '
+            if enfer:
+                razon = razon + '- Positivo por COVID-19 '
+            if cuar:
+                razon = razon + '- En cuarentena por examen '
+            sessionDB.execute("INSERT INTO visitas (id,nit,ndocumento,apellidos,fent,nombres,reason,rsocial,tapa,tdocumento,temp,veredict) VALUES({0},{1},{2},'{3}','{4}-{5}-{6}','{7}','{8}','{12}',{9},'{10}',{11},False)".format(i,ni,nd,ape,dia.year,dia.strftime("%m"),dia.strftime("%d"),nom,razon,tap,td,tem,rsol))
+    return
+
+def hVisitas(nd,td):
+    v = sessionDB.execute("SELECT * from visitas WHERE ndocumento = {0} and tdocumento = '{1}' allow filtering".format(nd,td))
+    visi = []
+    for obj in v:
+        if obj.veredict == True: b = "Aceptado"
+        else: b = "Denegado"
+        a = str(obj.fent.date().year)+"-"+str(obj.fent.date().month)+"-"+str(obj.fent.date().day)
+        pers = [obj.rsocial,a,b,obj.reason]
+        visi.append(pers)
+    return visi
